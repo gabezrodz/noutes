4440f36d37427945c847b3d94454226eb51b36b2
Gabriel Rodrigues <cgabrielrr@gmail.com>
2022-03-30 10:46:59 -0300

    Feat: adicionado comportamento de prompt ao form

--- src/components/notes/NoteForm/NoteForm.jsx
+++ src/components/notes/NoteForm/NoteForm.jsx
@@ -1,10 +1,12 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Card from "../../shared/Card";
 import LoadingSpinner from "../../animations";
 import { Actions, Control, Form, Loading } from "./styles";
 import { Button } from "../../shared/Buttons/styles";
+import { Prompt } from "react-router-dom";
 
 const NoteForm = (props) => {
+  const [isEntered, setIsEntered] = useState(false);
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -17,27 +19,39 @@ const NoteForm = (props) => {
     props.onAddNote({ author: enteredAuthor, text: enteredText });
   };
 
+  const formFocusHandler = () => {
+    setIsEntered(true)
+  };
+
+  const finishEnteringHandler = () =>{
+    setIsEntered(false)
+  }
+
   return (
-    <Card>
-      <Form onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <Loading>
-            <LoadingSpinner />
-          </Loading>
-        )}
-      </Form>
-      <Control>
-        <label htmlFor="Autor">Autor</label>
-        <input type="text" id="author" ref={authorInputRef} />
-      </Control>
-      <Control>
-        <label htmlFor="texto">Texto</label>
-        <textarea id="texto" rows="5" ref={textInputRef} />
-      </Control>
-      <Actions>
-        <Button>Adicionar Nota</Button>
-      </Actions>
-    </Card>
+    <>
+      <Prompt when={isEntered} message={(location)=> "Tem certeza que deseja sair? Todo texto digitado será perdido!"}/>
+      <Card>
+        <Form onFocus={formFocusHandler} onSubmit={submitFormHandler}>
+          {props.isLoading && (
+            <Loading>
+              <LoadingSpinner />
+            </Loading>
+          )}
+
+          <Control>
+            <label htmlFor="Autor">Autor</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </Control>
+          <Control>
+            <label htmlFor="texto">Texto</label>
+            <textarea id="texto" rows="5" ref={textInputRef} />
+          </Control>
+          <Actions>
+            <Button onClick={finishEnteringHandler}>Adicionar Nota</Button>
+          </Actions>
+        </Form>
+      </Card>
+    </>
   );
 };
 
--- src/pages/NewNote.jsx
+++ src/pages/NewNote.jsx
@@ -1,11 +1,15 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import NoteForm from "../components/notes/NoteForm";
 
 const NewNote = () => {
+  const history = useHistory();
   const addNoteHandler = (noteData) => {
-    console.log(noteData)
+    console.log(noteData);
+
+    history.push("/notes");
   };
 
-  return <NoteForm  onAddNote={addNoteHandler}/>;
+  return <NoteForm onAddNote={addNoteHandler} />;
 };
 
 export default NewNote;
